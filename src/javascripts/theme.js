2cc601c20ea571265d8a654265c03be3690afa2d
razvanMiu <miu.razvan28@gmail.com>
2019-04-17 14:35:51 +0300

    Update

--- src/javascripts/theme.js
+++ src/javascripts/theme.js
@@ -6,4 +6,15 @@ $(function () {
   new PurpleMine.SidebarToggler()
   new PurpleMine.HistoryTabs()
   new PurpleMine.MenuCollapse()
+
+  /* EEA fixes */
+  function moveElementAbove(el_1, el_2) {
+    $(el_1).next().filter(el_2).insertBefore(el_1)
+  }
+
+  function moveElementBelow(el_1, el_2) {
+    $(el_1).prev().filter(el_2).insertAfter(el_1)
+  }
+
+  moveElementAbove('.members.box', '.projects.box')
 })
