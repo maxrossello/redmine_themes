bdf1d2361c93c6a9c3e9fca35b3e6d17d631acd9
Massimo Rossello <massimo.rossello@azcomtech.com>
2020-01-15 23:15:49 +0100

    Bump version 4.1.0

--- init.rb
+++ init.rb
@@ -23,15 +23,15 @@ Redmine::Plugin.register :redmine_themes do
   name 'Redmine Themes plugin'
   author 'Massimo Rossello'
   description 'Brings a selection of Redmine themes'
-  version '4.0.0'
+  version '4.1.0'
   url 'https://github.com/maxrossello/redmine_themes.git'
   author_url 'https://github.com/maxrossello'
-  requires_redmine :version_or_higher => '4.0.0'
+  requires_redmine :version_or_higher => '4.1.0'
 
 end
 
 Rails.configuration.to_prepare do
-    Redmine::Plugin.find(:redmine_themes).requires_redmine_plugin :redmine_pluggable_themes, :version_or_higher => '4.0.0'
+    Redmine::Plugin.find(:redmine_themes).requires_redmine_plugin :redmine_pluggable_themes, :version_or_higher => '4.1.0'
 end
 
 
