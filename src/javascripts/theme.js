aa3f9b4b3aa2bd40d2c14fe4ea619261cb449558
David Ichim <ichim.david@gmail.com>
2020-03-04 16:34:47 +0200

    WIP setting payment field when editing or adding a new time entry   [ichim-david refs #114732]

--- src/javascripts/theme.js
+++ src/javascripts/theme.js
@@ -1,23 +1,25 @@
 $(function () {
   /* global PurpleMine */
-  'use strict'
+  'use strict';
 
   /* eslint-disable no-new */
-  new PurpleMine.SidebarToggler();
-  new PurpleMine.HistoryTabs();
-  new PurpleMine.MenuCollapse();
+  if (window.PurpleMine) {
+    new PurpleMine.SidebarToggler();
+    new PurpleMine.HistoryTabs();
+    new PurpleMine.MenuCollapse();
+  }
 
   /* EEA fixes */
   function moveElementAbove(el_1, el_2) {
     $(el_1).next().filter(el_2).insertBefore(el_1);
   }
 
-  function moveElementBelow(el_1, el_2) {
-    $(el_1).prev().filter(el_2).insertAfter(el_1);
-  }
+  // function moveElementBelow(el_1, el_2) {
+  //   $(el_1).prev().filter(el_2).insertAfter(el_1);
+  // }
 
   moveElementAbove('.members.box', '.projects.box');
-  
+
   // #106078 block search for anonymous
   function blockSearchAnonymous(){
     if (!$('#loggedas').length) {
@@ -30,15 +32,15 @@ $(function () {
         document.cookie='_redmine_eea=1; expires='+d.toUTCString()+';path=/';
     }
   }
-  
+
   blockSearchAnonymous();
 
   // Redmine 4.0.3 fixes, see #104603
   function fixHistoryView(){
       var length = $('#issue-changesets').nextUntil('#history').length;
       var i = 0;
-      while (i < length) { 
-        $('#history').prev().insertAfter('#history'); 
+      while (i < length) {
+        $('#history').prev().insertAfter('#history');
         i++;
       }
   }
@@ -47,11 +49,50 @@ $(function () {
 
   function editWikiQuickSearch() {
     var pathname = window.location.pathname;
-    if  ((pathname.match('/projects/.*/wiki/.*/edit') || pathname.match('/projects/.*/wiki/.*/rename')) && $('#wiki_page_parent_id').length > 0) {
+    if  ((pathname.match('/projects/.*/wiki/.*/edit') || pathname.match('/projects/.*/wiki/.*/rename')) && jQuery('#wiki_page_parent_id').length > 0) {
 	    $('#wiki_page_parent_id').select2();
 	    $('body').addClass('page_that_uses_select2');
     }
   }
-  
+
+  function prependPaymentId() {
+    var ticket_payment_id = jQuery("#issue_custom_field_values_71").val();
+    var $log_payment_id = jQuery("#time_entry_custom_field_values_36");
+
+    if (ticket_payment_id) {
+      $(".contextual").find('.icon-edit, .icon-comment').on('click', function(){
+        $log_payment_id.prop("value", ticket_payment_id);
+      });
+    }
+  }
+  prependPaymentId();
+
+  function prependPaymentIdOnTimeEntries() {
+    var is_on_time_entries = window.location.href.indexOf('time_entries/new') !== -1;
+    var api_key, site_url, issue_number;
+    if (is_on_time_entries) {
+      site_url = window.location.origin;
+      jQuery.get(site_url + "/my/api_key").done(function(res) {
+        api_key = jQuery(res).find('pre').text();
+        issue_number = $("#time_entry_issue_id").val();
+
+        jQuery.ajax(site_url + "/issues/" + issue_number + '.json', {
+          headers: {
+            'X-Redmine-API-Key': api_key.toString(),
+            'Content-Type': 'application/json'
+          },
+          processData: false,
+          dataType: 'json',
+
+          type: 'GET'
+        }).done(function(res){
+          console.log(res.issue.custom_fields[2].value)
+        });
+      });
+    }
+  }
+
+  prependPaymentIdOnTimeEntries();
+
   editWikiQuickSearch();
-})
+});
