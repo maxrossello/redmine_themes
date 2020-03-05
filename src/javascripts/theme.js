5673e6f51601e9d423a0e5cab8d4b86fb98889ea
David Ichim <ichim.david@gmail.com>
2020-03-05 18:48:25 +0200

    Refs #114732 changed the following:
    - when loading ticket json data search for payment id from custom
      fields instead of choosing the second one in case there will be
      new custom fields added
    - hide warning message when clicking on edit or reply as you
      could select something else and then click cancel so when you
      go back to edit you still get the warning even if the payment is
      the same

--- src/javascripts/theme.js
+++ src/javascripts/theme.js
@@ -59,10 +59,24 @@ $(function () {
   var $log_payment = jQuery("#time_entry_custom_field_values_36");
   var $ticket_payment = jQuery("#issue_custom_field_values_71");
   var ticket_payment_value = $ticket_payment.val();
+  var $log_payment_parent =  $log_payment.parent();
+  $log_payment_parent.before($(
+    '<div class="conflict hidden" id="wrong_payment">' +
+    '<strong>Different Payment Rerefence ID selected</strong>' +
+    '<div class="conflict-details conflict-journal">' +
+    'Be aware that you are using a different <em>Payment Reference ID</em> from what was defined in the ticket to use: ' +
+    '<em id="wrong_payment_correct_value"></em>' +
+    '</div>' +
+    '</div>'
+  ));
+  var $wrong_payment = $("#wrong_payment");
   var prefillPaymentId = function() {
     if (ticket_payment_value) {
       $(".contextual").find('.icon-edit, .icon-comment').on('click', function(){
         $log_payment.prop("value", ticket_payment_value);
+        if (!$wrong_payment.hasClass('hidden')) {
+          $wrong_payment.addClass('hidden');
+        }
       });
     }
   };
@@ -91,7 +105,14 @@ $(function () {
 
           type: 'GET'
         }).done(function(res){
-          ticket_payment_value = res.issue.custom_fields[2].value;
+          var time_ticket = res.issue.custom_fields.filter(function(el) {
+             return el.name.indexOf('Payment') !== -1;
+          });
+
+          if (!time_ticket.length) {
+            return;
+          }
+          ticket_payment_value =  time_ticket[0].value;
           var $wrong_payment_correct_value = $wrong_payment_correct_value || $("#wrong_payment_correct_value");
           $wrong_payment_correct_value.text(ticket_payment_value);
           $log_payment.val(ticket_payment_value);
@@ -101,17 +122,6 @@ $(function () {
   };
   prefillPaymentIdOnTimeEntriesPage();
 
-  var $log_payment_parent =  $log_payment.parent();
-  $log_payment_parent.before($(
-    '<div class="conflict hidden" id="wrong_payment">' +
-      '<strong>Different Payment Rerefence ID selected</strong>' +
-      '<div class="conflict-details conflict-journal">' +
-       'Be aware that you are using a different <em>Payment Reference ID</em> from what was defined in the ticket to use: ' +
-        '<em id="wrong_payment_correct_value"></em>' +
-      '</div>' +
-    '</div>'
-  ));
-  var $wrong_payment = $("#wrong_payment");
   var $wrong_payment_correct_value = $("#wrong_payment_correct_value");
   $wrong_payment_correct_value.text(ticket_payment_value);
   $log_payment.change(function(){
@@ -122,13 +132,10 @@ $(function () {
           $wrong_payment.removeClass('hidden');
         }
         else {
-
           $wrong_payment.toggleClass('hidden');
         }
       }
   });
-
-
 
   editWikiQuickSearch();
 });
