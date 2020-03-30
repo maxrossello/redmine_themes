dafd78392180bf870fa40bd4b91a5aa32fabc902
David Ichim <ichim.david@gmail.com>
2020-03-30 19:00:58 +0300

    Refs #114858 introduced a ThemeUtils.js file with defines that can be used by other js files:
    - moved logic of ticket 114732 into a separate PrefillPaymentId.js file
      which will be concatenated by the build script, this way this file
      is clear what logic it contains and can be refactored later if
      necessary
    - added number identifiers in order for the concat script to know the
    order in which the scripts should be concatenated

--- src/javascripts/theme.js
+++ src/javascripts/theme.js
@@ -56,33 +56,6 @@ $(function () {
     }
   }
 
-  /* #114732 prefill payment id when editing or adding a new time entry */
-  var $log_payment = jQuery("#time_entry_custom_field_values_36");
-  var $ticket_payment = jQuery("#issue_custom_field_values_71");
-  var ticket_payment_value = $ticket_payment.val();
-  var $log_payment_parent =  $log_payment.parent();
-  $log_payment_parent.before($(
-    '<div class="conflict hidden" id="wrong_payment">' +
-    '<strong>Different Payment Rerefence ID selected</strong>' +
-    '<div class="conflict-details conflict-journal">' +
-    'Be aware that you are using a different <em>Payment Reference ID</em> from what was defined in the ticket to use: ' +
-    '<em id="wrong_payment_correct_value"></em>' +
-    '</div>' +
-    '</div>'
-  ));
-  var $wrong_payment = $("#wrong_payment");
-  var prefillPaymentId = function() {
-    if (ticket_payment_value) {
-      $(".contextual").find('.icon-edit, .icon-comment').on('click', function(){
-        $log_payment.prop("value", ticket_payment_value);
-        if (!$wrong_payment.hasClass('hidden')) {
-          $wrong_payment.addClass('hidden');
-        }
-      });
-    }
-  };
-  prefillPaymentId();
-
   var site_url = window.location.origin;
   var current_url = window.location.href;
   var current_project_name = $(".current-project").text();
@@ -116,28 +89,6 @@ $(function () {
     }
   };
   check_for_localcache_freshness();
-
-  var getUserToken = (function() {
-    var dfd = $.Deferred();
-    var $logged_in = $("#loggedas");
-    if (!$logged_in.length) {
-      return dfd.reject();
-    }
-    var user_id = window.parseInt($logged_in.find('.user').attr('href').match(/\d+/g)[0]);
-    var user_api_key = session_storage && session_storage.getItem('t_logged_user_api_key_' + user_id);
-    if (user_api_key) {
-      return dfd.resolve({api_key: user_api_key});
-    }
-    jQuery.get(site_url + "/my/api_key").done(function(res) {
-      var $res = jQuery(res);
-      var api_key = $res.find('pre').text();
-      session_storage && session_storage.setItem('t_logged_user_api_key_' + user_id, api_key);
-      return dfd.resolve({
-        api_key: api_key
-      });
-    });
-    return dfd.promise();
-  }());
 
   var isAssignedUserDeveloper = function(token, user_obj) {
     var dfd = $.Deferred();
@@ -309,6 +260,7 @@ $(function () {
 
   var $issue_assigned_to = $("#issue_assigned_to_id");
   var $issue_assigned_to_parent = $issue_assigned_to.parent();
+  var getUserToken = window.PurpleMine.getUserToken();
 
   $issue_assigned_to.change(function(){
     var selected_option = $(this).find(':selected')[0];
@@ -391,57 +343,6 @@ $(function () {
     });
   }
 
-  var prefillPaymentIdOnTimeEntriesPage = function() {
-    var is_on_time_entries = current_url.indexOf('time_entries/new') !== -1;
-    var issue_number;
-    if (is_on_time_entries) {
-      getUserToken.then(function(res) {
-        issue_number = $("#time_entry_issue_id").val();
-
-        if (!issue_number) {
-          return;
-        }
-
-        jQuery.ajax(site_url + "/issues/" + issue_number + '.json', {
-          headers: {
-            'X-Redmine-API-Key': res.api_key.toString(),
-            'Content-Type': 'application/json'
-          },
-          processData: false,
-          dataType: 'json',
-
-          type: 'GET'
-        }).done(function(res){
-          var time_ticket = res.issue.custom_fields.filter(function(el) {
-             return el.name.indexOf('Payment') !== -1;
-          });
-
-          if (!time_ticket.length) {
-            return;
-          }
-          ticket_payment_value =  time_ticket[0].value;
-          var $wrong_payment_correct_value = $wrong_payment_correct_value || $("#wrong_payment_correct_value");
-          $wrong_payment_correct_value.text(ticket_payment_value);
-          $log_payment.val(ticket_payment_value);
-        });
-      });
-    }
-  };
-  prefillPaymentIdOnTimeEntriesPage();
-
-  var $wrong_payment_correct_value = $("#wrong_payment_correct_value");
-  $wrong_payment_correct_value.text(ticket_payment_value);
-  $log_payment.change(function(){
-      var value = $(this).val();
-      if (ticket_payment_value) {
-        if ( value && value !== ticket_payment_value) {
-          $wrong_payment.removeClass('hidden');
-        }
-        else {
-          $wrong_payment.toggleClass('hidden');
-        }
-      }
-  });
 
 
   editWikiQuickSearch();
