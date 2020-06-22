e44a707383ca551f24e02b174ba9cf728d097fd4
Massimo Rossello <massimo.rossello@azcomtech.com>
2020-06-22 22:32:03 +0200

    Removed former purplemine2

--- assets/themes/PurpleMine2_taqueci/src/javascripts/theme.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  /* global PurpleMine */
-  'use strict'
-
-  /* eslint-disable no-new */
-  new PurpleMine.SidebarToggler()
-  new PurpleMine.HistoryTabs()
-  new PurpleMine.MenuCollapse()
-})
