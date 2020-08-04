05929a21ca2679a7c719751b146c4e7dde7e7c7f
Massimo Rossello <massimo.rossello@azcomtech.com>
2020-08-04 23:14:16 +0200

    refresh script

---
+++ refresh.sh
@@ -0,0 +1,12 @@
+git fetch --all
+git pull -s subtree --allow-unrelated-histories alex master
+git pull -s subtree --allow-unrelated-histories custom-gitlab master
+git pull -s subtree --allow-unrelated-histories flat master
+git pull -s subtree --allow-unrelated-histories flatly_light master
+git pull -s subtree --allow-unrelated-histories gitmike master
+git pull -s subtree --allow-unrelated-histories nyss-gitlab master
+git pull -s subtree --allow-unrelated-histories pepper master
+git pull -s subtree --allow-unrelated-histories pixel-cookers master
+git pull -s subtree --allow-unrelated-histories progressive master
+git pull -s subtree --allow-unrelated-histories purplemine2 master
+git pull -s subtree --allow-unrelated-histories taskman master
